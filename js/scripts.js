5e927622a250680f9a0d63c46805579f5de70a0c
felipemaciel <felipemrvieira@gmail.com>
2017-06-25 14:18:58 -0300

    scrollmagic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $( document ).ready(function() {
-    scaleVideoContainer();
+  // var scene = document.getElementById('scene');
+  // var parallax = new Parallax(scene);
 
-    var scene = document.getElementById('scene');
-var parallax = new Parallax(scene);
+  var controller = new ScrollMagic.Controller();
+  var cena = new ScrollMagic.Scene({
+    triggerElement: "#logo-cacife",
+    duration: "60%"
+  })
+  .setClassToggle("#logo-cacife", "aparece")
+  .addIndicators()
+  .addTo(controller);
 
 
+
+
+
+
+  // var exibe = "$('#logo-cacife').removeClass( 'fadeIn' ).addClass( 'fadeOut' )";
+  // setTimeout(exibe, 6000);
+  // var exibe2 = "$('#logo-starfish').removeClass( 'esconde' ).addClass( 'fadeIn' )";
+  // setTimeout(exibe2, 9000);
+  //exibeMenu();
 });
 
-function scaleVideoContainer() {
-
-    var height = $(window).height() + 5;
-    var unitHeight = parseInt(height) + 'px';
-    $('.main').css('height',unitHeight);
-
-}
+// function exibeMenu() {
+//   var exibe = "$('#menu').removeClass( 'esconde' ).addClass( 'animated fadeIn' )";
+//   setTimeout(exibe, 6000);
+//
+// }
